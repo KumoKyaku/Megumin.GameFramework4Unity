b6a60f8928640c90265e3f8d16b88c91d49989ca
云却 <479813005@qq.com>
2022-02-07 10:14:23 +0800

    调整目录结构

---
+++ Runtime/Item/BDItem/BDItemInstanceInWorld.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Megumin.GameFramework;
+using Megumin.GameFramework.Interaction;
+
+namespace Megumin.GameFramework.Item.BD
+{
+    public class BDItemInstanceInWorld : ItemInstanceInWorld, IInteractionElement
+    {
+        public bool CreateInstance = false;
+
+        private void Awake()
+        {
+
+        }
+
+        private void Start()
+        {
+
+        }
+
+
+    }
+}

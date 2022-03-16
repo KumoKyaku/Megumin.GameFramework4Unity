b9abc1a6be277d328a11f5bc58b711e1695dace0
云却 <479813005@qq.com>
2022-03-16 13:26:37 +0800

    有时限的数值

---
+++ Runtime/Numerical/Timelimited.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Megumin.GameFramework.Numerical
+{
+    public interface ITimelimitedNumericalTimer
+    {
+        ValueTask Wait(float duration);
+    }
+
+    public class TempProp : IConsumable
+    {
+        public int ConsumableCount { get; set; }
+        public bool Use(int count, out int used)
+        {
+            if (ConsumableCount >= count)
+            {
+                ConsumableCount -= count;
+                used = count;
+                return true;
+            }
+            else
+            {
+                ConsumableCount = 0;
+                used = ConsumableCount;
+                return false;
+            }
+        }
+    }
+
+    public class TimelimitedProperty<T>
+    {
+        protected List<IConsumable> List { get; } = new List<IConsumable>();
+        public T Type;
+        public int Current;
+
+        public void Cal()
+        {
+            Current = 0;
+            foreach (var item in List)
+            {
+                Current += item.ConsumableCount;
+            }
+        }
+
+        public virtual void Add(IConsumable propInstance)
+        {
+            List.Add(propInstance);
+            //Cal();
+            Current += propInstance.ConsumableCount;
+            Debug.Log($"获得临时数值 {Type} 增加{propInstance.ConsumableCount} ，现有 {Current}");
+        }
+
+        public virtual void Remove(IConsumable propInstance)
+        {
+            List.Remove(propInstance);
+            //Cal();
+            Current -= propInstance.ConsumableCount;
+            Debug.Log($"失去临时数值 {Type}  减少{propInstance.ConsumableCount} ，现有 {Current}");
+        }
+
+        //public virtual Span<IConsumable> GetSortedUseTarget()
+        //{
+        //    return List.;
+        //}
+
+        public bool TryUse(int count)
+        {
+            if (Current >= count)
+            {
+                //TODO,排序。
+                foreach (var item in List)
+                {
+                    if (item.Use(count, out int used))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        count -= used;
+                    }
+                }
+
+                List.RemoveAll((item) =>
+                {
+                    if (item.ConsumableCount <= 0)
+                    {
+                        return true;
+                    }
+                    return false;
+                });
+
+                Cal();
+                Debug.Log($"使用临时数值 {Type}  减少{count} ，现有 {Current}");
+                return true;
+            }
+            return false;
+        }
+    }
+
+    public abstract class TimelimitedPropertyObject<P, T>
+        where P : TimelimitedProperty<T>, new()
+    {
+        public Dictionary<T, P> lifePorpDic { get; } = new Dictionary<T, P>();
+
+        public bool TryGet(T type, out P p) => lifePorpDic.TryGetValue(type, out p);
+
+        public P GetAutoAdd(T type)
+        {
+            if (lifePorpDic.TryGetValue(type, out var p))
+            {
+
+            }
+            else
+            {
+                p = new();
+                p.Type = type;
+                lifePorpDic.Add(type, p);
+            }
+
+            return p;
+        }
+
+        /// <summary>
+        /// 特殊属性，先在这里做，然后抽象到数值系统。
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="count"></param>
+        /// <param name="duration"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        public void PushTemp(T type, int count, float duration)
+        {
+            var p = GetAutoAdd(type);
+            Push(count, duration, p);
+        }
+
+        public abstract ITimelimitedNumericalTimer Timer { get; }
+
+        protected async void Push(int count, float duration, P p)
+        {
+            TempProp propInstance = new TempProp();
+            propInstance.ConsumableCount = count;
+            p.Add(propInstance);
+            await Timer.Wait(duration);
+            p.Remove(propInstance);
+        }
+    }
+
+
+
+}
+
+
---
+++ Standard/IConsumable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Megumin.GameFramework
+{
+    /// <summary>
+    /// 可消耗的
+    /// </summary>
+    public interface IConsumable
+    {
+        /// <summary>
+        /// 可消耗的数量
+        /// </summary>
+        int ConsumableCount { get; }
+
+        /// <summary>
+        /// 使用消耗
+        /// </summary>
+        /// <param name="count"></param>
+        /// <param name="used">实际使用的数量</param>
+        /// <returns>数量是否足够</returns>
+        bool Use(int count, out int used);
+    }
+}
+
+

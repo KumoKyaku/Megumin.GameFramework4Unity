33465c8d5a7faa504c7426b434f6f8f396f3dcbc
云却 <479813005@qq.com>
2023-12-06 11:30:25 +0800

    增加全局CursorState

---
+++ Runtime/CursorState.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Megumin
+{
+    /// <summary>
+    /// 全局鼠标状态控制器
+    /// </summary>
+    public static class CursorState
+    {
+        public static readonly AnyTrueControl Visible = new(onValueChangedKV: OnVisibleChange);
+        public static readonly AnyTrueControl Unlock = new(onValueChangedKV: OnUnlockChange);
+
+        static void OnVisibleChange((object, bool) newValue, (object, bool) oldValue)
+        {
+            Cursor.visible = newValue.Item2;
+            Debug.Log($"OnVisibleChange newValue:{newValue}    ----    oldValue:{oldValue}");
+        }
+
+        static void OnUnlockChange((object, bool) newValue, (object, bool) oldValue)
+        {
+            Cursor.lockState = newValue.Item2 ? CursorLockMode.None : CursorLockMode.Locked;
+            Debug.Log($"OnUnlockChange newValue:{newValue}    ----    oldValue:{oldValue}");
+        }
+
+        public static void Refresh(bool forceRaiseEvent = false)
+        {
+            Visible.Refresh(forceRaiseEvent);
+            Unlock.Refresh(forceRaiseEvent);
+        }
+    }
+}
+
+
+
